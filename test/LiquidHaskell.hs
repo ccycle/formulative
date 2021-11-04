09794ca1cc76c0a50aa2b0646a866d68550a4446
ccycle <ccycle713@gmail.com>
2021-11-04 09:45:27 +0900

    Delete safeDiv2

--- test/LiquidHaskell.hs
+++ test/LiquidHaskell.hs
@@ -10,23 +10,15 @@
 myHead (x : _) = x
 myHead [] = error "Fear not! 'twill ne'er come to pass"
 
-{-@ safeDiv :: Int -> { d:Int | d /= 0 } -> Int @-}
-safeDiv :: Int -> Int -> Int
-safeDiv n 0 = error "zero divide"
-safeDiv n d = n `div` d
-
-{-@ safeDiv2 :: (Eq a, Fractional a) => a -> { v: a | v /= 0 } -> a @-}
-safeDiv2 n 0 = error "div 0"
-safeDiv2 a b = a / b
+{-@ safeDiv :: (Eq a, Fractional a) => a -> { v: a | v /= 0 } -> a @-}
+safeDiv n 0 = error "div 0"
+safeDiv a b = a / b
 
 conds n m l
     | n == 0 && m /= 0 = putStrLn "n=0"
     | m == 0 = putStrLn "m=0"
-    | otherwise = putStrLn ("safeDiv: " ++ show (safeDiv2 n m)) >> putStrLn ("last input: " ++ show l)
+    | otherwise = putStrLn ("safeDiv: " ++ show (safeDiv n m)) >> putStrLn ("last input: " ++ show l)
 main = do
-    -- print $ safeDiv 2 1
-    -- print $ safeDiv2 1 1
-    -- print $ safeDiv2 0 1
     putStrLn "enter three numbers"
     putStrLn "1st:"
     (n :: Double) <- read <$> getLine
