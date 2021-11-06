0d9f5ee6cabc4d49c1d67c6283dfde1dea07fdc1
ccycle <ccycle713@gmail.com>
2021-11-07 02:38:43 +0900

    Stash

--- test/LiquidHaskell.hs
+++ test/LiquidHaskell.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
--- import Language.Haskell.Liquid.Prelude
--- import LambDEC.Calculation.Operator.Arithmetic.Class
+import LambDEC.Calculation.Operator.Arithmetic.Class
+
 -- import LambDEC.Calculation.Operator.VectorSpace.Class
 
 {-@ type NonEmpty a = {v:[a]| 0 < len v} @-}
@@ -14,10 +14,29 @@ myHead [] = error "Fear not! 'twill ne'er come to pass"
 safeDiv n 0 = error "div 0"
 safeDiv a b = a / b
 
+unsafeDiv a b = a / b
+
 conds n m l
     | n == 0 && m /= 0 = putStrLn "n=0"
     | m == 0 = putStrLn "m=0"
     | otherwise = putStrLn ("safeDiv: " ++ show (safeDiv n m)) >> putStrLn ("last input: " ++ show l)
+
+{-@ inline (<<>>) @-}
+{-@ inline unitElem @-}
+{-@ class TestMonoid a where
+    (<<>>) :: a -> {v:a|v/=0} -> a
+    unitElem :: a
+@-}
+
+instance TestMonoid Double where
+    (<<>>) = (/)
+    unitElem = 0
+
+class TestMonoid a where
+    (<<>>) :: a -> a -> a
+    unitElem :: a
+
+main :: IO ()
 main = do
     putStrLn "enter three numbers"
     putStrLn "1st:"
