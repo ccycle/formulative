34110203c0986d130240f58aff4cc371f6a2b25d
ccycle <ccycle713@gmail.com>
2021-12-11 15:18:46 +0900

    Change main

---
+++ app/Postprocess.hs
@@ -0,0 +1,9 @@
+module Main where
+
+main = print "calculation start"
+
+-- Configを読み取って出力パスなどを指定
+-- update :: Updatable a , Exportable a => a -> m a
+-- calculation = runReader (...) $ do
+--  xNew <- calculation x
+--  export path xNew
