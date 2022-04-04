ccf620e7f4e7d40d43bcd8f7b72802dac0d18132
ccycle <ccycle713@gmail.com>
2022-04-05 01:30:44 +0900

    Change app

--- app/Postprocess.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-main = print "calculation start"
-
--- Configを読み取って出力パスなどを指定
--- update :: Updatable a , Exportable a => a -> m a
--- calculation = runReader (...) $ do
---  xNew <- calculation x
---  export path xNew
--- app/Preprocess.hs
+++ app/Preprocess.hs
@@ -8,11 +8,6 @@ import OptDEC.Preprocess.DefaultValue
 import OptDEC.Preprocess.Exception
 import OptDEC.Preprocess.ReadSetting (writeDhallFile)
 
--- TODO: ファイルパスの解析(PathIOを使う)
--- TODO: コマンドライン引数から設定ファイルのパスを指定できるようにする
--- TODO: 入力ファイルの不正値チェック
--- dimensionOfField n | n == 0 = particle
---                    | otherwise = Field
 -- Fieldの場合
 --  - 座標データの読み込み
 --  - 設定ファイルからの入力値と座標データの長さが一致するかを確認
