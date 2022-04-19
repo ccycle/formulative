ace780f4ec2089b418fb90c84010cebd16769abc
ccycle <ccycle713@gmail.com>
2022-04-19 23:50:51 +0900

    Fix getObjectiveFunctionM

--- src/Formulative/Calculation/Internal/Class.hs
+++ src/Formulative/Calculation/Internal/Class.hs
@@ -8,14 +8,15 @@ import Control.Algebra
 import Control.Carrier.State.Strict
 import Data.Csv (DefaultOrdered, ToNamedRecord, ToRecord)
 import Formulative.Calculation.Algebra.Arithmetic.Class
+import Formulative.Calculation.Internal.Variable.Effect
 import Formulative.Calculation.Optimization.AugmentedLagrangian
 import Formulative.Calculation.VectorSpace.Class
 
 class (VectorSpace a) => HasObjectiveFunctionM m a where
     getObjectiveFunctionM :: m (a -> Scalar a)
-    default getObjectiveFunctionM :: (Has (State a) sig m, InnerProductSpace a, HasGradObjectiveFunctionM m a) => m (a -> Scalar a)
+    default getObjectiveFunctionM :: (Has (Variable a) sig m, InnerProductSpace a, HasGradObjectiveFunctionM m a) => m (a -> Scalar a)
     getObjectiveFunctionM = do
-        xOld <- get
+        (VariableOld xOld) <- getVariableOld @a
         gradf <- getGradientOfObjectiveFunctionM
         return $ \x -> (x .-. xOld) <.> gradf x
 
@@ -25,12 +26,6 @@ class (VectorSpace a) => HasGradObjectiveFunctionM m a where
 -- initial condition
 class HasInitialConditionM m a where
     getInitialConditionM :: m a
-
-runInitialConditionM :: forall a m b. (Monad m, HasInitialConditionM m a) => StateC a m b -> m b
-runInitialConditionM f = do
-    val <- getInitialConditionM
-    evalState val f
-
 class HasEqualityConstraintM m a where
     type EqualityConstraintType a :: *
     getEqualityConstraintM :: m (a -> EqualityConstraintType a)
@@ -39,8 +34,6 @@ class HasGradPenaltyM m a where
 
 class (HasInitialConditionM m a) => HasUpdateM m a where
     updateM :: a -> m a
-
-newtype Variable a = Variable a
 
 class HasDependentVariableGlobalM m a where
     type DependentVariableGlobalType a :: *
