c457e790c372d963e66319231168a4098c6c960d
ccycle <ccycle713@gmail.com>
2022-04-24 01:27:04 +0900

    Add scripts

---
+++ formulative-examples/visualization-scripts/make_database.py
@@ -0,0 +1,66 @@
+# coding: UTF-8
+import argparse
+import pandas as pd
+import numpy as np
+import pathlib
+import json
+from mergedeep import merge
+import subprocess
+import os
+import dhall
+from collections import OrderedDict
+import glob
+
+def getParentDirStr(pathStr):
+    p_rel = pathlib.Path(pathStr)
+    p_parent = p_rel.parent
+    return str(p_parent)
+def relPathToAbsPath(path):
+    p_rel = pathlib.Path(path)
+    p_abs = p_rel.resolve()
+    return str(p_abs)
+
+def dhallPathToDataFrame(path):
+    with open(path) as f:
+        s = f.read()
+    dhallToDict = dhall.loads(s)
+    parentPath = str(pathlib.Path(path).parent)
+    dhallToDict["export"]["output"] = parentPath
+
+    # add dependent parameter to dictionary
+    parentDir = getParentDirStr(path)
+    dependentParameterPath = os.path.join(parentDir,"dependentParamater.dhall")
+    if os.path.exists(dependentParameterPath):
+        with open(dependentParameterPath) as f1:
+            s1 = f1.read()
+        dhallToDict1 = dhall.loads(s1)
+        dhallToDict2 = {"equation":dhallToDict1}
+        d1 = merge(dhallToDict,dhallToDict2)
+    return pd.json_normalize(d1,sep="_")
+
+def dhallPathListToDataFrame(all_paths):
+    df = pd.concat((dhallPathToDataFrame(f) for f in all_paths))
+    return df
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="an example program")
+    parser.add_argument(
+        "--settingFilePath",
+        help='Relative path for setting file. example: --settingFilePath="output/*/setting.dhall"',
+        default = "output/*/setting.dhall"
+    )
+    parser.add_argument(
+        "--output",
+        help='output file. example: --output="output/_database.csv"',
+        default = "output/_database.csv"
+    )
+
+    args = parser.parse_args()
+    settingFilePath_ = args.settingFilePath
+    settingFilePathList = glob.glob(settingFilePath_)
+    data = dhallPathListToDataFrame(settingFilePathList).sort_index(axis=1)
+    outputPath_ = args.output
+    data.to_csv(outputPath_, index=False)
+    print("Successfully exported to "+relPathToAbsPath(outputPath_))
--- formulative-examples/visualization-scripts/query.py
+++ formulative-examples/visualization-scripts/query.py
@@ -3,12 +3,54 @@ import argparse
 import os
 import glob
 import re
+import pathlib
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-d","--databaseDir", help="relative path of database.csv")
-args = parser.parse_args()
-if args.databaseDir:
-    print("databaseDir")
-    dirname = os.path.dirname(args.databaseDir)
-# string (condition function) -> path
-# df.query('equation.a <= 1',engine='python')
+def getParentDirStr(pathStr):
+    p_rel = pathlib.Path(pathStr)
+    p_parent = p_rel.parent
+    return str(p_parent)
+
+def relPathToAbsPath(path):
+    p_rel = pathlib.Path(path)
+    p_abs = p_rel.resolve()
+    return str(p_abs)
+
+def str_to_df_query(df,x):
+    return df.query(x)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d","--databaseDir",
+     help="relative path of database",
+     default = "output/_database.csv"
+     )
+    parser.add_argument("-q","--queryExpr",
+     help="expression of query",
+     )
+    parser.add_argument("-f","--fileName",
+     help="target file",
+     default=""
+     )
+    parser.add_argument("-o","--output",
+     help="Whether to export the results of the query execution",
+     default="query_result.csv"
+     )
+    parser.add_argument("-o","--indices",
+     help="indices",
+     default="query_result.csv"
+     )
+    args = parser.parse_args()
+    dbPath =  args.databaseDir
+    queryStr =  args.queryExpr
+    df = pd.read_csv(dbPath)
+    df1 = str_to_df_query(df,queryStr)
+    # target
+    fileName1 = args.fileName
+    df2 = df1["export_output"].map(lambda x: x+"/"+fileName1)
+    print(df2)
+    outputPath = args.output
+    print(df2.to_string(index=False,header=False))
+    if outputPath != "":
+        df1.to_csv(outputPath)
+        print("Successfully exported to "+relPathToAbsPath(outputPath))
