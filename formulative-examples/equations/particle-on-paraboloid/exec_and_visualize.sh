7d03581d54b92b5f0044417d6c21dc193346b669
ccycle <ccycle713@gmail.com>
2022-06-06 15:34:51 +0900

    Remove scripts

--- formulative-examples/equations/particle-on-paraboloid/exec_and_visualize.sh
+++
@@ -1,5 +0,0 @@
-find ./settingFiles -name "*.dhall" -exec cabal exec -- particle-on-paraboloid -s {} \;
-python ../../visualization-scripts/create_database.py
-python ../../visualization-scripts/plot3d.py --data position.csv
-python ../../visualization-scripts/view_database.py -H  equation_xInit equation_pxInit -S equation_xInit -o "position.png"
-# python ../../visualization-scripts/view_database.py -H equation_a equation_xInit equation_pyInit -S equation_pyInit -q " export_outputDirectory.str.contains(\"76f3aa7e81855a911a627f715ac0f23c7e836063\") "
