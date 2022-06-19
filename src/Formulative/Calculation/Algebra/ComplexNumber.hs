5f8c37991e435bb193059a607f1844809e13b95a
ccycle <ccycle713@gmail.com>
2022-06-19 18:06:48 +0900

    Fix ComplexNumber

--- src/Formulative/Calculation/Algebra/ComplexNumber.hs
+++ src/Formulative/Calculation/Algebra/ComplexNumber.hs
@@ -1,24 +1,26 @@
-{-# LANGUAGE DeriveAnyClass #-}
+module Formulative.Calculation.Algebra.ComplexNumber (
+    module Data.Complex,
+    module Formulative.Calculation.Algebra.ComplexNumber,
+) where
 
-module Formulative.Calculation.Algebra.ComplexNumber where
-
+import Data.Complex hiding (conjugate)
 import Formulative.Calculation.Algebra.Arithmetic.Class
-import GHC.Generics
 import Prelude hiding (fromInteger)
 import qualified Prelude
 
-data ComplexNumber a = ComplexNumber a a
-    deriving (Show, Eq, Generic, Additive, AdditiveGroup)
+instance Additive a => Additive (Complex a)
+instance AdditiveGroup a => AdditiveGroup (Complex a)
 
-instance (AdditiveGroup a, Multiplicative a) => Multiplicative (ComplexNumber a) where
-    ComplexNumber a1 a2 .*. ComplexNumber b1 b2 = ComplexNumber (a1 .*. b1 .-. a2 .*. b2) (a2 .*. b1 .+. a1 .*. b2)
-    one = ComplexNumber one zero
+instance (AdditiveGroup a, Multiplicative a) => Multiplicative (Complex a) where
+    (:+) a1 a2 .*. (:+) b1 b2 = (:+) (a1 .*. b1 .-. a2 .*. b2) (a2 .*. b1 .+. a1 .*. b2)
+    one = (:+) one zero
+instance (Semiring a, AdditiveGroup a) => Semiring (Complex a) where
+    fromInteger x = (:+) (fromInteger x) (fromInteger x)
 
-instance (Semiring a, AdditiveGroup a) => Semiring (ComplexNumber a) where
-    fromInteger x = ComplexNumber (fromInteger x) (fromInteger x)
+instance (Ring a) => Ring (Complex a)
 
-instance (Ring a) => Ring (ComplexNumber a)
+instance (Eq a, Field a) => Field (Complex a) where
+    reciprocal (((:+) a b)) = (:+) (a ./. (a .*. a .+. b .*. b)) (negation b ./. (a .*. a .+. b .*. b))
+    fromRational' a = (:+) (fromRational' a) zero
 
-instance (Eq a, Field a) => Field (ComplexNumber a) where
-    reciprocal ((ComplexNumber a b)) = ComplexNumber (a ./. (a .*. a .+. b .*. b)) (negation b ./. (a .*. a .+. b .*. b))
-    fromRational' a = ComplexNumber (fromRational' a) zero
+conjugate (x :+ y) = x :+ negation y
